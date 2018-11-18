8ab98474e72cbcf5274743085e1d651cd9e7ce4c
johncris <johncris@localhost.localdomain>
2018-11-18 13:29:21 +0800

    makabuang kay dili ma upload ang picture

--- add_contact.php
+++ add_contact.php
@@ -9,9 +9,13 @@
 	$home_phone_number=$work_phone_number=$city=$zip_code=$state=$bio='';
 	$firstname_err= $lastname_err= $nickname_err= $profile_pic_err=$cell_phone_number_err='';
 	$home_phone_number_err=$work_phone_number_err=$city_err=$zip_code_err=$state_err=$bio_err='';
-
+	$profile='';
 
 	if(isset($_POST['submit']) && $_SERVER["REQUEST_METHOD"] == 'POST'){
+		
+		//$profile_pic = $_FILES['profile_pic']['name'];
+		//$profile_tmp = $_FILES['profile_pic']['tmp_name'];
+		//move_uploaded_file($profile_tmp, "img/$profile");
 
 		if(isset($_POST['firstname'])){
 			$firstname = mysqli_real_escape_string($conn ,$_POST['firstname']); 
@@ -24,17 +28,24 @@
 		if(isset($_POST['nickname'])){
 			$lastnamnicknamee = mysqli_real_escape_string($conn ,$_POST['nickname']); 
 		}
+		if(isset($_POST['profile_pic'])){
+			$profile_pic = mysqli_real_escape_string($conn ,$_POST['profile_pic']); 
+
+			
+		}
+		 
 		
 		
+		/*
 		if(isset($_FILES['profile_pic'])){
 			
-			$profile_pic = addslashes(file_get_contents($_FILES["profile_pic"]["tmp_name"]));  
+			//$profile_pic = addslashes(file_get_contents($_FILES["profile_pic"]["tmp_name"]));  
       
 			//$profile_pic = $_FILES['profile_pic']['name'];
 			//$profile_pic_tmp = $_FILES['profile_pic']['tmp_name'];
 			
 			//move_uploaded_file($profile_pic_tmp, "img/$profile_pic");
-			/*
+			
 			$profile_pic = $_FILESp['profile_pic']['tmp_name'];
 			$profile_pic = getimagesize($profile_pic);
 			
@@ -45,8 +56,17 @@
 				$profile = addslashes(file_get_contents($profile));
 				saveimage($image);
 
-			}*/
+			}
+			
+	}*/
+	/*
+		//$check = getimagesize($_FILES["profile_pic"]["tmp_name"]);
+		if(getimagesize($_FILES["profile_pic"]["tmp_name"]) !== false){
+			$image = $_FILES['profile_pic']['tmp_name'];
+			$profile_pic = addslashes(file_get_contents($image));
+
 		}
+	*/
 		
 		
 		if(empty(trim($_POST["cell_phone_number"]))&&isset($_POST['cell_phone_number'])){
@@ -96,13 +116,31 @@
 			$bio = mysqli_real_escape_string($conn ,$_POST['bio']); 
 		}
 
+		$output_dir = home_url()."img";
+
+if(isset($_FILES["profile_pic"])){
+    if ($_FILES["profile_pic"]["error"] > 0){
+      echo "Error: " . $_FILES["file"]["error"] . "<br>";
+    }else{
+        move_uploaded_file($_FILES["profile_pic"]["tmp_name"],$output_dir. $_FILES["profile_pic"]["name"]);
+        /*if(get_user_meta($_SESSION['userid'], 'user_profile_picture')==""){
+            add_user_meta($_SESSION['userid'], 'user_profile_picture', $_FILES['profile_pic']);
+        }else{
+            update_user_meta($_SESSION['userid'], 'user_profile_picture', $_FILES['profile_pic']);
+        }*/
+        echo "Uploaded File :".$_FILES["profile_pic"]["name"];
+    }
+}
 
 
 		if(empty($cell_phone_number_err)){
             
 
 			//you can save contact with cellphone number only
+			//$profile 		= $_FILES['profile_pic']['name'];
+			//$profile_tmp 	= $_FILES['profile_pic']['tmp_name'];
 
+			//move_uploaded_file($profile_tmp, "img/$profile");
 
             $sql = "INSERT INTO contacts (First_Name,Last_Name,Nickname,Phone_Number,Work_Phone_Number,Home_Phone_Number,City,State,ZIpcode,Profile_Picture,Bio,User_ID) VALUES 
 			('$firstname','$lastname','$nickname','$cell_phone_number','$work_phone_number','$home_phone_number','$city','$state','$zip_code','$profile_pic','$bio','$user_id')";
@@ -159,7 +197,7 @@
 		</div>  
 		<div class="form-group <?php echo (!empty($profile_pic_err)) ? 'has-error' : ''; ?>">
 			<label>Profile</label>
-			<input type="file" name="profile_pic" id="profile_pic" class="form-control" value="<?php echo $profile_pic; ?>">
+			<input type="file" name="profile_pic" id="profile_pic" class="form-control">
 			<span class="help-block"><?php echo $profile_pic_err; ?></span>
 		</div>  
 		<div class="form-group <?php echo (!empty($cell_phone_number_err)) ? 'has-error' : ''; ?>">
@@ -205,75 +243,7 @@
 		</div>
 		 
 	</form>
-
-
-		<!-- content section -->
-		<div class="content">
-		<div><h1>Create New Contact</h1></div>
-			<hr>
-			<div class="contact">
-				<div class="contact_insert">
-					<form action="add_contact.php" method="post" enctype="multipart/form-data">
-						<table style="float:left" width="50%">
-							<tr>
-								<td>First Name:</td>
-								<td><input type="text" name="fname" placeholder="First Name"  size="40%"></td>
-							</tr>
-							<tr>
-								<td>Last Name:</td>
-								<td><input type="text" name="lname" placeholder="Last Name" size="40%"></td>
-							</tr>
-							<tr>
-								<td>Nickname:</td>
-								<td><input type="text" name="nickname" placeholder="Nickname" size="40%"></td>
-							</tr>
-							<tr>
-								<td>Profile Image:</td>
-								<td><input type="file" name="profile"></td>
-							</tr>
-							<tr>
-								<td>Cell Phone:</td>
-								<td><input type="text" name="cphone" placeholder="Cell Phone" size="40%"></td>
-							</tr>
-							<tr>
-								<td>Home Phone:</td>
-								<td><input type="text" name="hphone" placeholder="Home Phone" size="40%"></td>
-							</tr>
-							<tr>
-								<td>Work Phone:</td>
-								<td><input type="text" name="wphone" placeholder="Work Phone" size="40%"></td>
-							</tr>
-							<tr>
-								<td>Address:</td>
-								<td><input type="text" name="address" placeholder="Address" size="40%"></td>
-							</tr>
-							<tr>
-								<td>City:</td>
-								<td><input type="text" name="city" placeholder="City" size="40%"></td>
-							</tr>
-							<tr>
-								<td>State:</td>
-								<td><input type="text" name="state" placeholder="State" size="40%"></td>
-							</tr>
-							<tr>
-								<td>Zipcode:</td>
-								<td><input type="text" name="zipcode" placeholder="Zipcode" size="40%"></td>
-							</tr>
-						</table>
-						<table style="float:right" width="45%">
-							<tr>
-								<td>Bio:</td>
-								<td><textarea name="bio" id="bio" cols="30" rows="10"></textarea></td>
-							</tr>
-						</table>
-						<div class="clear"></div>
-						<input type="submit" name="submit" id="submit" value="Insert Contact">
-						<a href="index.php"><input class="cancel_contact_button" type="button" value="Cancel"></a>
-					</form>
-				</div>
-				<div class="clear"></div>
-			</div>
-		</div>	
+	
 </body>
 </html>	
 
@@ -300,3 +270,4 @@
       });  
  });  
  </script>  
+ 
