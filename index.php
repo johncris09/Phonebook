3159861c63fec873900c6d03dd6b4c066f792b4d
johncris <johncris@localhost.localdomain>
2018-11-18 17:13:00 +0800

    can now view data

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
 <?php
    include('session.php');
-   include('config.php');
    
-
-	$contacts = array();
-
-	$all_contacts = "select * from contacts where contact_status = '1'";
-
-	$sql_all_contacts = $conn->query($all_contacts);
-
-	$total_contacts = $sql_all_contacts->num_rows;
-
-	while ($row = mysqli_fetch_assoc($sql_all_contacts)) {
-		$contacts[] = $row;
-	}
+   $user_id = $_SESSION['user_id'];
+   
+   $result = mysqli_query($conn, "Select Contact_ID, CONCAT(First_Name,' ', Last_Name) as Name, Nickname, Phone_Number from contacts where User_ID ='$user_id'");
+   
  
 ?>
 
@@ -28,30 +19,40 @@
 
 		<!-- content section -->
 		<div class="content">
-			<div class="floatl"><h1><?php echo $total_contacts ?> Contact(s) Total</h1></div>
+			
 			<a class="floatr" href="insert_contact.php"><input class="cancel_contact_button" type="button" value="New Contact"></a>		
-			<div class="clear"></div>
-				<hr class="pageTitle">
-				<table id="contactsTable" class="display">
-					<thead>
-						<tr align="left">
-							<th>Name:</th>
-							<th>Nickname:</th>
-							<th>Cell Phone:</th>
-							<th>Actions</th>
-						</tr>
-					</thead>
-					<tbody>
-					 	<?php foreach ($contacts as $contact) {?>
-						<tr>
-							<td><a href="contact.php?id=<?php echo $contact["contact_id"]; ?>"><?php echo $contact["contact_fname"] . " " . $contact["contact_lname"]; ?></a></td>
-							<td><?php echo $contact["contact_nickname"]; ?></td>
-							<td><?php echo $contact["contact_cphone"]; ?></td>
-							<td><a href="update_contact.php?id=<?php echo $contact["contact_id"]; ?>"><i class="fa fa-pencil"></i></a> | <a href="delete_contact.php?id=<?php echo $contact["contact_id"] ?>"><i class="fa fa-trash-o"></i></a></td>
-						</tr>
-						<?php } ?>
-					</tbody>
-				</table>
+			<table style="width:100%;text-align:left; border: 1px solid black">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Nickname</th>
+						<th>Phone Number</th>
+						<th>Operation</th>
+						
+					</tr>
+				</thead>
+				<tbody>
+					<?php while ($row = mysqli_fetch_array($result)) { ?>
+					<tr>
+						<td><?php echo $row['Name'] ?></td>
+						<td><?php echo $row['Nickname'] ?></td>
+						<td><?php echo $row['Phone_Number'] ?></td>
+						<td>
+							<a href="view_contact.php?id=<?php echo $row["Contact_ID"]; ?>">View</a> |
+							<a href="update_contact.php?id=<?php echo $row["Contact_ID"]; ?>">Update</a> | 
+							<a href="delete_contact.php?id=<?php echo $row["Contact_ID"] ?>">Delete</i></a>
+						</td>
+
+					</tr>
+					<?php } ?>
+
+
+
+				</tbody>
+
+			</table>
+			
+			
 		</div>
 	</div>	
 </body>
