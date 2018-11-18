89156e76b3e6e09608a3f0c0ac32e0239e64f19c
johncris <johncris@localhost.localdomain>
2018-11-18 16:13:53 +0800

    tnx god . its working

--- insert_contact.php
+++ insert_contact.php
@@ -2,7 +2,71 @@
     session_start();
     include('config.php');
     $user_id = $_SESSION['user_id'];
- ?>
+
+    $cell_phone_number_err="";
+
+
+	if (isset($_POST['submit'])) {
+        $firstname = $_POST['fname'];
+		$lastname = $_POST['lname'];
+        $nickname = $_POST['nickname'];
+        $profile_pic='';
+		$profile= $_FILES['profile']['name'];
+        $profile_tmp = $_FILES['profile']['tmp_name'];
+        $cell_phone_number = $_POST["cphone"];
+		$home_phone_number = $_POST['hphone'];
+		$work_phone_number = $_POST['wphone'];
+		$city = $_POST['city'];
+		$state = $_POST['state'];
+		$zip_code = $_POST['zipcode'];
+        $bio = $_POST['bio'];
+
+
+        
+        if(empty(trim($cell_phone_number)) ){
+            $cell_phone_number_err = "Please enter a Cell Phone Number";
+        } else{
+            
+            $cell_phone_number = mysqli_real_escape_string($conn,$cell_phone_number);
+
+			$sql = "SELECT * FROM contacts WHERE Phone_Number='".$cell_phone_number."'";
+			
+            $result = mysqli_query($conn,$sql);
+            $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+            
+            $count = mysqli_num_rows($result);
+
+            if($count == 1) {
+                $cell_phone_number_err = "This cellphone number is already taken.";
+            }else {
+                
+                $cell_phone_number = trim($_POST["cphone"]);
+            }
+            
+        }
+
+        // Moving Picture to new directory
+		move_uploaded_file($profile_tmp, "img/$profile");
+       
+        if(empty($cell_phone_number_err)){
+            $sql = "INSERT INTO contacts (First_Name,Last_Name,Nickname,Phone_Number,Work_Phone_Number,Home_Phone_Number,City,State,ZIpcode,Profile_Picture,Bio,User_ID) VALUES 
+            ('$firstname','$lastname','$nickname','$cell_phone_number','$work_phone_number','$home_phone_number','$city','$state','$zip_code','$profile','$bio','$user_id')";
+
+            if (mysqli_query($conn, $sql)) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+
+        }
+        
+        mysqli_close($conn);
+
+	}
+
+ 
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,14 +75,11 @@
 <body>
 	<div class="wrapper">
 
-		<!-- header section -->
-		<div class="header">
-			<div class="headerContent"><h1>CONTACT LIST</h1></div>
-		</div>
+		
 
 		<!-- content section -->
 		<div class="content">
-		<div><h1>Create New Contact</h1></div>
+		Create new Contact
 			<hr>
 			<div class="contact">
 				<div class="contact_insert">
@@ -42,8 +103,11 @@
 							</tr>
 							<tr>
 								<td>Cell Phone:</td>
-								<td><input type="text" name="cphone" placeholder="Cell Phone" size="40%"></td>
-							</tr>
+                                <td><input type="text" name="cphone" placeholder="Cell Phone" size="40%">
+                                <span><?php echo $cell_phone_number_err; ?></span>
+                                </td>
+                            
+                            </tr>
 							<tr>
 								<td>Home Phone:</td>
 								<td><input type="text" name="hphone" placeholder="Home Phone" size="40%"></td>
@@ -67,17 +131,18 @@
 							<tr>
 								<td>Zipcode:</td>
 								<td><input type="text" name="zipcode" placeholder="Zipcode" size="40%"></td>
-							</tr>
-						</table>
-						<table style="float:right" width="45%">
-							<tr>
+                            </tr>
+                            <tr>
 								<td>Bio:</td>
 								<td><textarea name="bio" id="bio" cols="30" rows="10"></textarea></td>
-							</tr>
-						</table>
+                            </tr>
+                            <tr>
+                                <td><input class="insert_contact_button" type="submit" name="submit" value="Insert Contact"></td>
+                                <td><a href="index.php"><input class="cancel_contact_button" type="button" value="Cancel"></a></td>
+                            </tr>
 						<div class="clear"></div>
-						<input class="insert_contact_button" type="submit" name="submit" value="Insert Contact">
-						<a href="index.php"><input class="cancel_contact_button" type="button" value="Cancel"></a>
+						
+						
 					</form>
 				</div>
 				<div class="clear"></div>
@@ -85,43 +150,3 @@
 		</div>	
 </body>
 </html>		
-<?php 
-
-	if (isset($_POST['submit'])) {
-        
-        
-        
-        
-		$firstname = $_POST['fname'];
-		$lastname = $_POST['lname'];
-        $nickname = $_POST['nickname'];
-        $profile_pic='';
-		$profile= $_FILES['profile']['name'];
-		$profile_tmp = $_FILES['profile']['tmp_name'];
-		$cell_phone_number = $_POST['cphone'];
-		$home_phone_number = $_POST['hphone'];
-		$work_phone_number = $_POST['wphone'];
-		$city = $_POST['city'];
-		$state = $_POST['state'];
-		$zip_code = $_POST['zipcode'];
-        $bio = $_POST['bio'];
-
-        if(isset($_POST['profile_pic'])){
-			$profile_pic = mysqli_real_escape_string($conn ,$_POST['profile_pic']);	
-        }
-
-		move_uploaded_file($profile_tmp, "img/$profile");
-       
-        $sql = "INSERT INTO contacts (First_Name,Last_Name,Nickname,Phone_Number,Work_Phone_Number,Home_Phone_Number,City,State,ZIpcode,Profile_Picture,Bio,User_ID) VALUES 
-        ('$firstname','$lastname','$nickname','$cell_phone_number','$work_phone_number','$home_phone_number','$city','$state','$zip_code','$profile_pic','$bio','$user_id')";
-
-        if (mysqli_query($conn, $sql)) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-        }
-        mysqli_close($conn);
-
-	}
-
- ?>
