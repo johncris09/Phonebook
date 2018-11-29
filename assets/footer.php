25a1e0cbd4081c8b746e7f4ba2e4039b24d98747
johncris <johncris@localhost.localdomain>
2018-11-29 22:10:47 +0800

    myfooter

---
+++ assets/footer.php
@@ -0,0 +1,22 @@
+<footer>
+    <div class="row">
+        <div class="col-sm-4 col-sm-offset-4">
+            <h3 class="text-center">Follow Me</h3>        
+            <ul>
+                <li><a href="https://web.facebook.com/profile.php?id=100006745398153" class="active"><img src="img/social_icons/fb.png" alt="facebook"/></a></li>
+                <li><a href="mailto:jcmanabo@gmail.com" class="active"><img src="img/social_icons/gmail.png" alt="gmail"/></a></li>
+                <li><a href="https://twitter.com/JohnCri14289540" class="active"><img src="img/social_icons/twitter.png" alt="twitter"/></a></li>
+                <li><a href="https://github.com/johncris09" class="active"><img src="img/social_icons/github.svg" alt="github"/></a></li>
+            
+            </ul>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="copyright text-center">
+                <p>Copyright @2018 | Designed With by <a target="_blank" href="https://github.com/johncris09">John Cris</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
